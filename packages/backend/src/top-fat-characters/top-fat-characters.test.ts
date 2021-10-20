c7f0b88804435f522288ba061f67ced0a5fcfb64
Sofia Johansson <sofia5@kth.se>
2021-10-20 14:37:39 +0200

    added another test case

--- packages/backend/src/top-fat-characters/top-fat-characters.test.ts
+++ packages/backend/src/top-fat-characters/top-fat-characters.test.ts
@@ -7,4 +7,14 @@ describe('top-fat-characters', () => {
     expect(response.status).toEqual(200);
     expect(response.type).toEqual('application/json');
   });
+
+  it('existing star wars character should be included in data', async () => {
+    const response = await request(app.callback()).get('/top-fat-characters');
+    const existingStarwarsCharacter = { name: 'Han Solo' };
+    expect(response.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining(existingStarwarsCharacter),
+      ])
+    );
+  });
 });
